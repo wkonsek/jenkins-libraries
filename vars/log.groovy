7e8418e5a871674ed1ac99b7c82df2d3ac9aac8f
Wojciech Konsek <wojciech.konsek@bright-it.com>
2018-02-22 13:31:59 +0100

    changes in vars

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,9 +1,13 @@
-def test = 'test'
-
 def info(message) {
     echo "INFO: ${message}"
 }
 
 def warning(message) {
     echo "WARNING: ${message}"
+}
+
+def set(){
+    environment {
+    	TEST='test'
+    }
 }
